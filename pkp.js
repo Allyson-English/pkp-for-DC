7b87c066990a98080d60ea03a4ceba574a420cce
Allyson English <58833140+Allyson-English@users.noreply.github.com>
2020-08-14 11:18:44 -0400

    correcting mobile nav scroll

--- pkp.js
+++ pkp.js
@@ -1,13 +1,21 @@
 function navigation() {
 
-   if (document.getElementById("drop_menu").value === 'issues') {
-   	location.hash = "issues"}
+   if (document.getElementById("drop_menu").value === 'home') {
+      location.hash = "top";
+      document.getElementById("drop_menu").value = "menu"}
+   else if (document.getElementById("drop_menu").value === 'issues') {
+   	location.hash = "issues";
+      document.getElementById("drop_menu").value = "menu"}
    else if (document.getElementById("drop_menu").value === 'aboutme') {
-   	location.hash = "aboutme"}
+   	location.hash = "aboutme";
+      document.getElementById("drop_menu").value = "menu"}
    else if (document.getElementById("drop_menu").value === 'ANC') {
-   	location.hash = "ANC"}
+   	location.hash = "ANC";
+      document.getElementById("drop_menu").value = "menu"}
    else if (document.getElementById("drop_menu").value === 'intouch') {
-   	location.hash = "intouch"}
+   	location.hash = "intouch";
+      document.getElementById("drop_menu").value = "menu"}
    else if (document.getElementById("drop_menu").value === 'donate') {
-   	location.hash = "donate"}
-}
+   	location.hash = "donate";
+      document.getElementById("drop_menu").value = "menu"}
+};
